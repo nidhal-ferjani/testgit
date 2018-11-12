2fbf12ba7e5674692754326917adab3fab9cdb95
Nidhal Ferjani <nidhal.ferjani@gmail.com>
2018-11-12 22:09:32 +0100

    modification d'affichage

--- src/test/git/GitMyLife.java
+++ src/test/git/GitMyLife.java
@@ -3,7 +3,7 @@ package test.git;
 public class GitMyLife {
 
 	public static void main(String[] args) {
-		System.out.println("je suis la bien sur");
+		System.out.println("je suis la bien sur 3000");
 	}
 
 }
